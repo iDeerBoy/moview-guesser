04ed825117128538b3f2ce2400b788831b6701c6
Jimmy Pulido <jpulido@bch.bancodechile.cl>
2026-04-13 11:24:30 -0500

    add wating for players

--- src/components/organisms/ScoreBoard/Scoreboard.tsx
+++ src/components/organisms/ScoreBoard/Scoreboard.tsx
@@ -7,6 +7,11 @@ const ScoreBoard = () => {
         <h4>Scoreboard</h4>
         <span>Add another player, it’s sad playing alone :(</span>
       </div>
+      <div className={styles.scoreboard__Body}>
+        <p>
+          Waiting for players<span>...</span>
+        </p>
+      </div>
     </section>
   );
 };
